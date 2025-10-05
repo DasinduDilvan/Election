00d6b30434b9107fb9b6fadd1859cf8703ed95ea
madukamanaram <madukamanaram@gmail.com>
2025-10-05 22:47:58 +0530

    maduka - devoloper(pull this)

--- Election_C/src/voter/voter_registration.c
+++ Election_C/src/voter/voter_registration.c
@@ -4,15 +4,15 @@ const char *COLOR = "\033[1;33m";
 const char *CLRRM = "\033[0m";
 
 void showHeader();
-char voterID();
-char fname();
-char lname();
-char username();
-char password();
-char NIC();
-char gender();
-char dob();
-char area();
+char *voterID();
+char *fname();
+char *lname();
+char *username();
+char *password();
+char *NIC();
+char *gender();
+char *dob();
+char *area();
 char confirm();
 
 int main(){
@@ -38,7 +38,7 @@ int main(){
     else {
         printf("Registration confirmed.\n");
 
-    FILE *file = fopen("../../../database/source_data/voters.txt", "a");
+    FILE *file = fopen("../../database/source_data/voters.txt", "a");
     if (file == NULL) {
         printf("Error opening file!\n");
         return 1;
@@ -87,76 +87,76 @@ void showHeader(){
     printf("╠══════════════════════════════════════════════════════════════════════════╝\n");
 }
 
-char voterID(){
-    char voterID[20];
+char *voterID(){
+    static char voterID[20];
     printf("║\n");
     printf("╠═ Enter Voter ID: ");
-    scanf("%s", voterID);
-    return *voterID;
+    scanf("%19s", voterID);
+    return voterID;
 }
 
-char fname(){
-    char fname[20];
+char *fname(){
+    static char fname[20];
     printf("║\n");
     printf("╠═ Enter First Name: ");
-    scanf("%s", fname);
-    return *fname;
+    scanf("%19s", fname);
+    return fname;
 }
 
-char lname(){
-    char lname[20];
+char *lname(){
+    static char lname[20];
     printf("║\n");
     printf("╠═ Enter Last Name: ");
-    scanf("%s", lname);
-    return *lname;
+    scanf("%19s", lname);
+    return lname;
 }
 
-char username(){
-    char username[20];
+char *username(){
+    static char username[20];
     printf("║\n");
     printf("╠═ Enter Username: ");
-    scanf("%s", username);
-    return *username;
+    scanf("%19s", username);
+    return username;
 }
 
-char password(){
-    char password[20];
+char *password(){
+    static char password[20];
     printf("║\n");
     printf("╠═ Enter Password: ");
-    scanf("%s", password);
-    return *password;
+    scanf("%19s", password);
+    return password;
 }
 
-char NIC(){
-    char NIC[20];
+char *NIC(){
+    static char NIC[20];
     printf("║\n");
     printf("╠═ Enter NIC: ");
-    scanf("%s", NIC);
-    return *NIC;
+    scanf("%19s", NIC);
+    return NIC;
 }
 
-char gender(){
-    char gender[10];
+char *gender(){
+    static char gender[10];
     printf("║\n");
     printf("╠═ Enter Gender(m/f): ");
-    scanf("%s", gender);
-    return *gender;
+    scanf("%9s", gender);
+    return gender;
 }
 
-char dob(){
-    char dob[15];
+char *dob(){
+    static char dob[15];
     printf("║\n");
     printf("╠═ Enter Date of Birth(DD/MM/YYYY): ");
-    scanf("%s", dob);
-    return *dob;
+    scanf("%14s", dob);
+    return dob;
 }
 
-char area(){
-    char area[30];
+char *area(){
+    static char area[30];
     printf("║\n");
     printf("╠═ Enter Area: ");
-    scanf("%s", area);
-    return *area;
+    scanf("%29s", area);
+    return area;
 }
 
 char confirm(){
@@ -166,3 +166,4 @@ char confirm(){
     scanf(" %c", &confirm);
     return confirm;
 }
+
