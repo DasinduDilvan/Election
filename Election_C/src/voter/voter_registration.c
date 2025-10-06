282c3fa336a694a108dc8ea0d9b3865cda14837b
DasinduDilvan <dilwanwije@gmail.com>
2025-10-06 07:23:15 +0530

    new void functions add for show the last massage in voter_reg section

--- Election_C/src/voter/voter_registration.c
+++ Election_C/src/voter/voter_registration.c
@@ -4,16 +4,18 @@ const char *COLOR = "\033[1;33m";
 const char *CLRRM = "\033[0m";
 
 void showHeader();
-char voterID();
-char fname();
-char lname();
-char username();
-char password();
-char NIC();
-char gender();
-char dob();
-char area();
+char *voterID();
+char *fname();
+char *lname();
+char *username();
+char *password();
+char *NIC();
+char *gender();
+char *dob();
+char *area();
 char confirm();
+void massageSuccess();
+void massageCanceled();
 
 int main(){
     showHeader();
@@ -31,43 +33,35 @@ int main(){
     printf("╠═ Confirm Registration (y/n): ");
     scanf(" %c", &confirm_save);
 
-    if (confirm_save != 'y' && confirm_save != 'Y') {
-        printf("╠═ Registration cancelled.\n");
-        return 0;
+    if (confirm_save == 'y' && confirm_save == 'Y') {
+
+        printf("║\n");
+        printf("╠═ Registration confirmed.\n");
+
+            FILE *file = fopen("../../database/source_data/voters.txt", "a");
+            if (file == NULL) {
+                printf("Error opening file!\n");
+                return 1;
+            }
+            fprintf(file,"%s\n", voter_ID);
+            fprintf(file,"%s\n", firstname);
+            fprintf(file,"%s\n", lastname);
+            fprintf(file,"%s\n", user_name);
+            fprintf(file,"%s\n", pass_word);
+            fprintf(file,"%s\n", NIC_num);
+            fprintf(file,"%s\n", gender_mf);
+            fprintf(file,"%s\n", Dateofbirth);
+            fprintf(file,"%s\n", election_area);
+            fclose(file);
+
+            massageSuccess();
+            
     }
     else {
-        printf("Registration confirmed.\n");
-
-    FILE *file = fopen("../../../database/source_data/voters.txt", "a");
-    if (file == NULL) {
-        printf("Error opening file!\n");
-        return 1;
-    }
-    fprintf(file,"%s\n", voter_ID);
-    fprintf(file,"%s\n", firstname);
-    fprintf(file,"%s\n", lastname);
-    fprintf(file,"%s\n", user_name);
-    fprintf(file,"%s\n", pass_word);
-    fprintf(file,"%s\n", NIC_num);
-    fprintf(file,"%s\n", gender_mf);
-    fprintf(file,"%s\n", Dateofbirth);
-    fprintf(file,"%s\n", election_area);
-    fclose(file);
-
-    FILE *ffff = fopen("../../../database/source_data/voters.txt", "r");
-    if (ffff == NULL) {
-        printf("Error opening file!\n");
-        return 1;
-    }
-    char test[100];
-    fscanf(ffff,"%s\n", test);
-    printf("%s\n", test);
-    fclose(file);
-
-    printf("\n");
-    printf("╠══════════════════════════════════════════════════════════════════════════╗\n");
-    printf("║                       %sRegistration Successful!%s                           ║\n", COLOR, CLRRM);
-    printf("╚══════════════════════════════════════════════════════════════════════════╝\n");
+        printf("║\n");
+        printf("╠═ Registration cancelled.\n");
+        massageCanceled();
+        return 0;    
     }
 return 0;
 }
@@ -87,82 +81,88 @@ void showHeader(){
     printf("╠══════════════════════════════════════════════════════════════════════════╝\n");
 }
 
-char voterID(){
-    char voterID[20];
+char *voterID(){
+    static char voterID[20];
     printf("║\n");
     printf("╠═ Enter Voter ID: ");
-    scanf("%s", voterID);
-    return *voterID;
+    scanf("%19s", voterID);
+    return voterID;
 }
 
-char fname(){
-    char fname[20];
+char *fname(){
+    static char fname[20];
     printf("║\n");
     printf("╠═ Enter First Name: ");
-    scanf("%s", fname);
-    return *fname;
+    scanf("%19s", fname);
+    return fname;
 }
 
-char lname(){
-    char lname[20];
+char *lname(){
+    static char lname[20];
     printf("║\n");
     printf("╠═ Enter Last Name: ");
-    scanf("%s", lname);
-    return *lname;
+    scanf("%19s", lname);
+    return lname;
 }
 
-char username(){
-    char username[20];
+char *username(){
+    static char username[20];
     printf("║\n");
     printf("╠═ Enter Username: ");
-    scanf("%s", username);
-    return *username;
+    scanf("%19s", username);
+    return username;
 }
 
-char password(){
-    char password[20];
+char *password(){
+    static char password[20];
     printf("║\n");
     printf("╠═ Enter Password: ");
-    scanf("%s", password);
-    return *password;
+    scanf("%19s", password);
+    return password;
 }
 
-char NIC(){
-    char NIC[20];
+char *NIC(){
+    static char NIC[20];
     printf("║\n");
     printf("╠═ Enter NIC: ");
-    scanf("%s", NIC);
-    return *NIC;
+    scanf("%19s", NIC);
+    return NIC;
 }
 
-char gender(){
-    char gender[10];
+char *gender(){
+    static char gender[10];
     printf("║\n");
     printf("╠═ Enter Gender(m/f): ");
-    scanf("%s", gender);
-    return *gender;
+    scanf("%9s", gender);
+    return gender;
 }
 
-char dob(){
-    char dob[15];
+char *dob(){
+    static char dob[15];
     printf("║\n");
     printf("╠═ Enter Date of Birth(DD/MM/YYYY): ");
-    scanf("%s", dob);
-    return *dob;
+    scanf("%14s", dob);
+    return dob;
 }
 
-char area(){
-    char area[30];
+char *area(){
+    static char area[30];
     printf("║\n");
     printf("╠═ Enter Area: ");
-    scanf("%s", area);
-    return *area;
+    scanf("%29s", area);
+    return area;
 }
 
-char confirm(){
-    char confirm;
+void massageSuccess(){
     printf("║\n");
-    printf("╠═ Confirm Registration (y/n): ");
-    scanf(" %c", &confirm);
-    return confirm;
+    printf("╠══════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                        %sRegistration Successful!%s                          ║\n", COLOR, CLRRM);
+    printf("╚══════════════════════════════════════════════════════════════════════════╝\n");
+}
+
+void massageCanceled(){
+    printf("║\n");
+    printf("╠══════════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                         %sRegistration Canceled!%s                           ║\n", COLOR, CLRRM);
+    printf("╚══════════════════════════════════════════════════════════════════════════╝\n");
 }
