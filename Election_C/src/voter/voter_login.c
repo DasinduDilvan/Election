c16cab6a1b3b7ff80342a889d0d4a4cd744e6ec9
DasinduDilvan <dilwanwije@gmail.com>
2025-10-08 17:17:23 +0530

    make the connection with

--- Election_C/src/voter/voter_login.c
+++ Election_C/src/voter/voter_login.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 const char *COLOR = "\033[1;33m";
 const char *CLRRM = "\033[0m";
 
-int option;
+void showHeader();
+void showContent();
 
 int main(){
 
+    system("clear || cls");
+    showHeader();
+
+    return 0;
+}
+
+void showHeader() {
     printf("\n");
     printf("╔══════════════════════════════════════════════════════════════════════════╗\n");
     printf("║                                                                          ║\n");
@@ -17,6 +27,6 @@ int main(){
     printf("║      %s███████ ███████ ███████  ██████    ██    ██  ██████  ██   ████%s      ║\n", COLOR, CLRRM);
     printf("║                                                                          ║\n");
     printf("╠══════════════════════════════════════════════════════════════════════════╣\n");
-
-    return 0;
+    printf("║                                Admin Login                               ║\n");
+    printf("╠══════════════════════════════════╦═══════════════════════════════════════╣\n");
 }
